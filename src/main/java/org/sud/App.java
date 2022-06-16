eb3d4ad0ebd83c267548aa23199a2ad6a754124b
Sudarsana Ala <sudarsana.ala@gmail.com>
2022-06-16 19:47:40 +0530

    first commit

---
+++ src/main/java/org/sud/App.java
@@ -0,0 +1,13 @@
+package org.sud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
