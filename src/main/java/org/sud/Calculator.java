3042c690c5e9c12c47f339641c1baa4dc9ee6519
Sud <sudarsana.ala@gmail.com>
2023-02-06 10:49:20 +0530

    added vuln code

--- src/main/java/org/sud/Calculator.java
+++ src/main/java/org/sud/Calculator.java
@@ -1,13 +1,44 @@
 package org.sud;
 
+
+import java.io.Console;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  * Hello world!
  *
  */
 public class Calculator
 {
-    String username = "dummy";
+    
     public Integer add(Integer num1, Integer num2){
         return num1 + num2;
     }
+
+    public boolean test(){
+        String username = System.console().readLine();
+        String password = System.console().readLine();
+        // Unsafe query which uses string concatenation
+        String query = "select * from tbluser where username='" + username + "' and password = '" + password + "'";
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/user", "root", "root");
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                stmt.close();
+                conn.close();
+            } catch (Exception e) {}
+        }
+        return true;
+
+    }
+
 }
