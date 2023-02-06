da1f3d792399aa7c63f5834a31b60207259423ef
Sud <sudarsana.ala@gmail.com>
2023-02-06 15:15:55 +0530

    conn - no null check

--- src/main/java/org/sud/Calculator.java
+++ src/main/java/org/sud/Calculator.java
@@ -1,5 +1,9 @@
 package org.sud;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,15 @@ public class Calculator
     String username = "dummy";
     public Integer add(Integer num1, Integer num2){
         return num1 + num2;
+    }
+
+    public void test() throws SQLException {
+        Connection conn = null;
+
+        try{
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/user", "root", "root");
+        }catch (Exception e){
+            conn.close();
+        }
     }
 }
